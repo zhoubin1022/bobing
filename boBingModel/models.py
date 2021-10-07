c3a9fc7c64ad098c928e30e1931dac91664b588c
zhoubin1022 <2031348494@qq.com>
2021-10-07 12:26:57 +0800

    后端：对代码进行了注释，方便阅读

--- boBingModel/models.py
+++ boBingModel/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 
 class Player(models.Model):
-    name = models.CharField(max_length=10)
-    photo = models.ImageField(upload_to="photos", default="a.jpg")
-    level = models.SmallIntegerField(null=True)
+    name = models.CharField(max_length=10)  # 昵称
+    photo = models.ImageField(upload_to="photos", default="a.jpg")  # 头像
+    level = models.SmallIntegerField(null=True)  # 最高奖项
 
 
 class Record(models.Model):
-    pid = models.SmallIntegerField()
-    round = models.SmallIntegerField()
-    playerNum = models.SmallIntegerField()
-    nowLevel = models.SmallIntegerField()
-    uid = models.ForeignKey('Player', on_delete=models.CASCADE)
+    pid = models.SmallIntegerField()  # 本轮游戏id
+    round = models.SmallIntegerField()  # 轮数
+    playerNum = models.SmallIntegerField()  # 玩家人数
+    nowLevel = models.SmallIntegerField()  # 本局游戏最高奖项
+    uid = models.ForeignKey('Player', on_delete=models.CASCADE)  # 参与玩家id，必须在player中出现
--- boBingModel/views.py
+++ boBingModel/views.py
@@ -1,8 +1,6 @@
-import json
 import os
 import random
 import re
-from pathlib import Path
 
 from django.core import serializers
 from django.http import JsonResponse, HttpResponse
@@ -10,6 +8,7 @@ from boBingModel.models import Player, Record
 from bobing import settings
 
 rules = [
+
     "114444",  # 状元插金花 0
     "444444",  # 六杯红 1
     "111111",  # 遍地锦 2
@@ -27,31 +26,31 @@ rules = [
 ]
 
 
-def doRandom():
+def doRandom():  # 产生随机数列表，即骰子点数
     ran = []
     for i in range(6):
         ran.append(random.randint(1, 6))
     return ran
 
 
-def judge(request):
+def judge(request):  # 用于判断点数对应的奖项
     if request.method == 'GET':
         result = {"message": 'success', "data": []}
         ran = doRandom()
         result["data"].append({})
-        for i in range(len(ran)):
+        for i in range(len(ran)):  # 生成json
             result["data"][0][f"num_{i+1}"] = ran[i]
-        ran.sort()
+        ran.sort()  # 排序，便于匹配
         ran_str = ''
-        for i in ran:
+        for i in ran:  # 将列表转为字符串
             ran_str = ran_str + str(i)
         level = 12
-        for x in range(len(rules)):
+        for x in range(len(rules)):  # 逐一匹配正则
             re_str = rules[x]
             if re.compile(re_str).match(ran_str):
                 level = x
                 break
-        result["data"][0]["level"] = level
+        result["data"][0]["level"] = level  # 奖项
         # print(result_ran)
         # print(level)
         return JsonResponse(result)
@@ -71,12 +70,12 @@ def allPlayer(request):
 def addPlayer(request):
     # 添加玩家
     if request.method == 'POST':
-        photo = request.FILES['photo']
+        photo = request.FILES['photo']  # 获取信息
         name = request.POST.get('name')
-        users = Player.objects.filter(name__exact=name)
+        users = Player.objects.filter(name__exact=name)  # 判断该昵称是否存在
         if users.exists():
             return JsonResponse({"message": '该昵称已存在'})
-        if photo:
+        if photo:  # 判断该照片之前是否接受过
             file_info, suffix = os.path.splitext(photo.name)
             if suffix.upper() not in ['.JPG', '.JPEG', '.PNG']:
                 return JsonResponse({"message": '图片格式不正确，只接受jpg、jpeg、png格式的图片'})
@@ -86,25 +85,25 @@ def addPlayer(request):
             if os.path.exists(path):
                 os.remove(path)
                 print("remove")
-        new_player = Player(name=name, photo=photo)
+        new_player = Player(name=name, photo=photo)  # 保存玩家
         new_player.save()
-        new_user = Player.objects.filter(name__exact=name)
+        new_user = Player.objects.filter(name__exact=name)  # 返回添加后信息
         return JsonResponse({"message": 'success', "data": serializers.serialize('python', new_user)})
     return JsonResponse({"message": 'wrong'})
 
 
 def editPlayer(request):  # 编辑玩家信息
     if request.method == 'POST':
-        photo = request.FILES['photo']
+        photo = request.FILES['photo']  # 获取请求信息
         name = request.POST.get('name')
         uid = request.POST.get('id')
-        users = Player.objects.filter(name=name)
+        users = Player.objects.filter(name=name)  # 判断该昵称是否重复
         # return JsonResponse({"message": 'success', "data": serializers.serialize('python', users)})
         if users.exists():
             for user in users:
                 if not (user.id == uid):
                     return JsonResponse({"message": '该昵称已存在'})
-        users = Player.objects.get(id=uid)
+        users = Player.objects.get(id=uid)  # 判断图片是否存在，名字一样就视为图片一样
         if photo:
             photo_info, suf = os.path.splitext(photo.name)
             if suf.upper() not in ['.JPG', '.JPEG', '.PNG']:
@@ -113,10 +112,10 @@ def editPlayer(request):  # 编辑玩家信息
             path = os.path.join(path, 'photos')
             path = os.path.join(path, photo.name)
             if not os.path.exists(path):
-                users.photo = photo
+                users.photo = photo  # 不一样则修改头像
             users.name = name
-            users.save()
-            users = Player.objects.filter(name__exact=name)
+            users.save()  # 保存信息
+            users = Player.objects.filter(name__exact=name)  # 返回修改后信息
             return JsonResponse({"message": 'success', "data": serializers.serialize('python', users)})
     return JsonResponse({"message": 'wrong'})
 
@@ -124,7 +123,7 @@ def editPlayer(request):  # 编辑玩家信息
 def deletePlayer(request):
     # 删除玩家
     if request.method == 'POST':
-        uid = request.POST.get('id')
+        uid = request.POST.get('id')  # 根据id找到对应玩家并删除
         users = Player.objects.get(id=uid)
         users.delete()
         return JsonResponse({"message": 'success'})
@@ -134,18 +133,18 @@ def deletePlayer(request):
 def partPlayer(request):
     # 模糊查询玩家
     if request.method == 'POST':
-        part_name = request.POST.get('str')
+        part_name = request.POST.get('str')  # 获取模糊信息
         result = {"message": 'success', "data": []}
-        users = Player.objects.filter(name__contains=part_name)
-        result["data"] = serializers.serialize('python', users)
+        users = Player.objects.filter(name__contains=part_name)  # 查找
+        result["data"] = serializers.serialize('python', users)  # 返回
         return JsonResponse(result)
     return JsonResponse({"message": 'wrong'})
 
 
-def allRecord(request):
+def allRecord(request):  # 返回某个玩家所有记录
     if request.method == 'POST':
         uid = request.POST.get('id')
-        records = Record.objects.filter(uid__exact=uid)
+        records = Record.objects.filter(uid__exact=uid)  # 获取记录
         result = {"message": 'success', "data": serializers.serialize('python', records)}
         return JsonResponse(result)
     return JsonResponse({"message": 'wrong'})
@@ -153,7 +152,7 @@ def allRecord(request):
 
 def addRecord(request):  # 添加记录
     if request.method == 'POST':
-        pid = request.POST.get('pid')
+        pid = request.POST.get('pid')  # 获取记录信息
         lun = request.POST.get('lun')
         num = request.POST.get('num')
         level = request.POST.get('level')
@@ -164,6 +163,7 @@ def addRecord(request):  # 添加记录
             user.level = level
             user.save()
         new_record = Record.objects.create(pid=pid, round=lun, playerNum=num, nowLevel=level, uid_id=uid)
+        # 产生新记录
         # new_record = Record(int(pid), int(lun), int(num), int(level), uid_id=user.pk)
         new_record.save()
         records = Record.objects.filter(pid=pid).filter(uid_id=uid)
@@ -175,20 +175,20 @@ def allRank(request):
     # 返回排行榜
     if request.method == 'GET':
         result = {"message": 'success', "data": []}
-        users = Player.objects.filter(level__isnull=False).order_by('level')
-        for user in users:
+        users = Player.objects.filter(level__isnull=False).order_by('level')  # 根据Player中的level信息排序返回排名
+        for user in users:  # 简化一下信息
             user_rank = {"name": user.name, "level": user.level}
             result["data"].append(user_rank)
         return JsonResponse(result)
     return JsonResponse({"message": 'wrong'})
 
 
-def nowRank(request):
+def nowRank(request):  # 本轮游戏排行榜
     if request.method == 'POST':
-        pid = request.POST.get('id')
+        pid = request.POST.get('id')  # 游戏id
         result = {"message": 'success', "data": []}
-        records = Record.objects.filter(pid=pid).order_by('nowLevel')
-        for record in records:
+        records = Record.objects.filter(pid=pid).order_by('nowLevel')  # 根据id获取记录并排序
+        for record in records:  # 简化信息
             user_rank = {}
             user = Player.objects.get(id=record.uid_id)
             user_rank["name"] = user.name
